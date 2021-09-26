4b82699033dbb7a6722ed8f74c5e174164810c63
Akos Kemives <akoskm@gmail.com>
2021-09-26 14:47:09 +0200

    add login form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Input,
+  Stack,
+  FormControl,
+  Button,
+  Flex,
+  Heading,
+  FormErrorMessage,
+  Alert,
+  AlertIcon,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSuccessful, setIsSuccessful] = useState(false);
+  const [isInvalid, setIsInvalid] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+
+    if (!username) {
+      setIsInvalid(true);
+      setErrorMessage("Email is required");
+      return;
+    }
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then(({ success }) => {
+        if (!success) {
+          setErrorMessage("Login failed.");
+        }
+        setIsSuccessful(success);
+        setIsInvalid(!success);
+      });
+  }
+
+  function handleOnChange(e) {
+    const { name, value } = e.target;
+
+    if (name === "username") {
+      setUsername(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Flex
+        flexDirection="column"
+        width="100wh"
+        height="100vh"
+        backgroundColor="gray.200"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Heading color="teal.400">Welcome</Heading>
+        <form onSubmit={handleOnSubmit} autoComplete="off">
+          <Stack
+            spacing={4}
+            p="1rem"
+            backgroundColor="whiteAlpha.900"
+            boxShadow="md"
+          >
+            <FormControl>
+              <Input
+                name="username"
+                type="text"
+                placeholder="Email"
+                onChange={handleOnChange}
+                value={username}
+                autoComplete="new-password"
+              />
+            </FormControl>
+            <FormControl>
+              <Input
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={handleOnChange}
+                value={password}
+              />
+            </FormControl>
+            <FormControl>
+              <Button
+                borderRadius={0}
+                type="submit"
+                variant="solid"
+                colorScheme="teal"
+                width="full"
+              >
+                Login
+              </Button>
+            </FormControl>
+            <FormControl>
+              {isInvalid && (
+                <Alert status="error">
+                  <AlertIcon />
+                  {errorMessage}
+                </Alert>
+              )}
+              {isSuccessful && (
+                <Alert status="success">
+                  <AlertIcon />
+                  Login successful!
+                </Alert>
+              )}
+            </FormControl>
+          </Stack>
+        </form>
+      </Flex>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,85 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import userEvent from "@testing-library/user-event";
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders the form", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("form submission", () => {
+  test("validates the email field", () => {
+    render(<App />);
+    expect(screen.queryByText(/Email is required/i)).not.toBeInTheDocument();
+
+    const submitButton = screen.getByText(/Login/i);
+    submitButton.click();
+
+    expect(screen.queryByText(/Email is required/i)).toBeInTheDocument();
+  });
+
+  test("login successful", async () => {
+    window.fetch = jest.fn().mockResolvedValue({
+      json: () => ({
+        success: true,
+      }),
+    });
+
+    render(<App />);
+
+    // fill the form
+    const emailInput = screen.getByPlaceholderText(/Email/i);
+    userEvent.type(emailInput, "user@example.com");
+
+    const passwordInput = screen.getByPlaceholderText(/Password/i);
+    userEvent.type(passwordInput, "abc123");
+
+    const submitButton = screen.getByText(/Login/i);
+    submitButton.click();
+
+    await waitFor(() =>
+      expect(screen.queryByText(/Login successful!/i)).toBeInTheDocument()
+    );
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+    expect(window.fetch).toHaveBeenCalledWith(
+      "https://jsonplaceholder.typicode.com/posts",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          username: "user@example.com",
+          password: "abc123",
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+  });
+
+  describe("login failed", () => {
+    test("failure message", async () => {
+      // stub fetch
+      window.fetch = jest.fn().mockResolvedValue({
+        json: () => ({
+          success: false,
+        }),
+      });
+
+      render(<App />);
+
+      const emailInput = screen.getByPlaceholderText(/Email/i);
+      userEvent.type(emailInput, "user@example.com");
+
+      const passwordInput = screen.getByPlaceholderText(/Password/i);
+      userEvent.type(passwordInput, "abc123");
+
+      const submitButton = screen.getByText(/Login/i);
+      submitButton.click();
+
+      await waitFor(() =>
+        expect(screen.queryByText(/Login failed./i)).toBeInTheDocument()
+      );
+    });
+  });
 });
